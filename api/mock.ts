190fb4c258e7690466c11b588ca36ca2fef7f830
Anton <merkulovich166@gmail.com>
2025-10-24 16:48:07 +0300

    feature: test vercel deploy pt. 2

--- api/mock.ts
+++ api/mock.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const db = JSON.parse(readFileSync(path.join(__dirname, '../db.json'), 'utf-8'));
+const db = JSON.parse(readFileSync(path.join(__dirname, '/db.json'), 'utf-8'));
 
 const router = createRouter();
 
